1c8700e03dd6453d38f4c28e6ea9553b0a20293e
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-22 15:03:12 -0500

    main game loop working

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+
+    let correct = true
+
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+    if(correct) renderNewQuote()
+})
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
@@ -6,9 +32,15 @@ function getRandomQuote() {
     .then(data => data.content)
 }
 
-async function getNextQuote() {
+async function renderNewQuote() {
     const quote = await getRandomQuote()
-    console.log(quote)
+    quoteDisplayElement.innerText = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null
 }
 
-getNextQuote();
+renderNewQuote();
