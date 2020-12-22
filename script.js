e0110b8095deb76a013a15ff3a9fd0301ad79094
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-22 15:13:31 -0500

    added time function, finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
@@ -41,6 +42,21 @@ async function renderNewQuote() {
         quoteDisplayElement.appendChild(characterSpan)
     })
     quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime
+
+function startTimer(){
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval( () => {
+    timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime(){
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
 renderNewQuote();
